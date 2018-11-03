039be082e9f697ad60cf9700eed9a5523b1309a1
Nutod <alukodotun@gmail.com>
2018-11-03 22:54:17 +0100

    Updates

---
+++ precache-manifest.23c028c610087c96f5593cd9bf99e97c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "96c09c0a7b068e10a874",
+    "url": "/portfolio/static/js/main.96c09c0a.chunk.js"
+  },
+  {
+    "revision": "a68f4f8b40152f1dc0b4",
+    "url": "/portfolio/static/js/1.a68f4f8b.chunk.js"
+  },
+  {
+    "revision": "96c09c0a7b068e10a874",
+    "url": "/portfolio/static/css/main.9ee7dd73.chunk.css"
+  },
+  {
+    "revision": "f2347273c8ecfcaae3e9058b7d4ee928",
+    "url": "/portfolio/index.html"
+  }
+];

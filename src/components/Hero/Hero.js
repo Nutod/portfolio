740d28bef5300cce271935af58f7806ab8985878
Nutod <alukodotun@gmail.com>
2018-11-10 12:46:40 +0100

    Adjusted the Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,9 @@
 import React from "react";
+import ReactTypingEffect from 'react-typing-effect';
 import { HeadingOne, HeadingThree, HeroHeader } from "../elements/Headers";
 import { HeroWrapper, HeroBackground } from "../elements/Wrapper";
 import { Button } from "../elements/Buttons";
+import './Hero.css'
 
 export default function Hero() {
 	// const Text = ['Frontend Developer', 'UI Designer']
@@ -10,6 +12,10 @@ export default function Hero() {
 			<div style={{ width: "80vw" }}>
 				<HeadingThree>Hi! I'm</HeadingThree>
 				<HeroHeader>Oladotun Aluko</HeroHeader>
+				<ReactTypingEffect
+				className="HeadingOne"
+      text="Hello." //text=["Hello.", "World!"]
+    />
 				<HeadingOne>Frontend Developer</HeadingOne>
 				<p style={{ marginTop: "2.7rem" }}>
 					<Button href="Résume.pdf" download="Oladotun Aluko Résume">Download Resume</Button>

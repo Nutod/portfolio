2421ca81c11c0d218b8325264a6c11212b6b501c
Nutod <alukodotun@gmail.com>
2018-11-10 12:59:29 +0100

    Added typingConfig object

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
-import { HeadingOne, HeadingThree, HeroHeader } from "../elements/Headers";
+import { HeadingThree, HeroHeader } from "../elements/Headers";
 import { HeroWrapper, HeroBackground } from "../elements/Wrapper";
 import { Button } from "../elements/Buttons";
 import "./Hero.css";
 
 export default function Hero() {
 	const Text = ["Frontend Developer", "UI/UX Developer"];
+	const typingConfig = {
+		text: Text,
+		className: "HeadingOne",
+		speed: 100,
+		eraseDelay: 3000,
+		cursor: "_"
+	};
 
 	return (
 		<HeroWrapper>
 			<div style={{ width: "80vw" }}>
 				<HeadingThree>Hi! I'm</HeadingThree>
 				<HeroHeader>Oladotun Aluko</HeroHeader>
-				<ReactTypingEffect className="HeadingOne" text={Text} />
+				<ReactTypingEffect {...typingConfig} />
 				<p style={{ marginTop: "2.7rem" }}>
 					<Button href="Résume.pdf" download="Oladotun Aluko Résume">
 						Download Resume

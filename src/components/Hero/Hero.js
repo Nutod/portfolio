6440df62cc7b284a6c971fa5b4314d223ddae139
Nutod <alukodotun@gmail.com>
2018-11-10 12:52:32 +0100

    Added scrolling effect

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,17 +6,14 @@ import { Button } from "../elements/Buttons";
 import "./Hero.css";
 
 export default function Hero() {
-	// const Text = ['Frontend Developer', 'UI Designer']
+	const Text = ["Frontend Developer", "UI/UX Developer"];
+
 	return (
 		<HeroWrapper>
 			<div style={{ width: "80vw" }}>
 				<HeadingThree>Hi! I'm</HeadingThree>
 				<HeroHeader>Oladotun Aluko</HeroHeader>
-				<ReactTypingEffect
-					className="HeadingOne"
-					text="Hello." //text=["Hello.", "World!"]
-				/>
-				<HeadingOne>Frontend Developer</HeadingOne>
+				<ReactTypingEffect className="HeadingOne" text={Text} />
 				<p style={{ marginTop: "2.7rem" }}>
 					<Button href="Résume.pdf" download="Oladotun Aluko Résume">
 						Download Resume

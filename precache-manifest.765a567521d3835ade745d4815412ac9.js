f0fd2ab61950aac57ecf0bf4ae582d306b3f54ea
Nutod <alukodotun@gmail.com>
2018-11-19 01:08:46 +0100

    Updates

---
+++ precache-manifest.765a567521d3835ade745d4815412ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "d492e4c8c0f96a946917",
+    "url": "/portfolio/static/js/main.d492e4c8.chunk.js"
+  },
+  {
+    "revision": "e16b073860b244e1ec4c",
+    "url": "/portfolio/static/js/1.e16b0738.chunk.js"
+  },
+  {
+    "revision": "d492e4c8c0f96a946917",
+    "url": "/portfolio/static/css/main.5e6c3b0a.chunk.css"
+  },
+  {
+    "revision": "d912d938952be853bdd945c615f630da",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.454a66a9b09a2d55181c418be4c11194.js"
+  "/portfolio/precache-manifest.765a567521d3835ade745d4815412ac9.js"
 );
 
 workbox.clientsClaim();

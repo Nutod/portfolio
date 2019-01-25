d82a14f9b76e67d22618ef8954482f9706a2d147
Nutod <alukodotun@gmail.com>
2019-01-25 13:44:50 +0100

    Added new todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Layout from "./containers/Layout";
 
+// TODO: Add tests to Components
+// TODO: Add ErrorBoundary
+// TODO: Add CI
 class App extends Component {
 	render() {
 		return <Layout />;

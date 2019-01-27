d9eaa0c88c3eaee3ee8ebaec89dc5aacc727bd6e
Nutod <alukodotun@gmail.com>
2019-01-27 22:50:09 +0100

    Added ErrorBoundary Component to Layout

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -3,14 +3,15 @@ import Hero from "../components/Hero/Hero";
 import Experience from "../components/Experience/Experience";
 import SideProject from "../components/SideProject/SideProject";
 import Footer from "../components/Footer/Footer";
+import ErrorBoundary from "../hoc/ErrorBoundary";
 
 export default function Layout() {
 		return (
-			<>
+			<ErrorBoundary>
 				<Hero />
 				<Experience />
 				<SideProject />
 				<Footer />
-			</>
+			</ErrorBoundary>
 		);
 }
